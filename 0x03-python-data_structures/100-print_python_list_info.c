22ded7bc7c834b7aaa453c6d96f54c6f074577ee
ImisiDahunsi4 <136653498+ImisiDahunsi4@users.noreply.github.com>
2024-01-09 01:58:48 +0100

    committer

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,14 @@
+#include <python.h>
+#include <object.h>
+#include <listobject.h>
+void print_python_list_info(PyObject *p)
+{
+	long int size = PyList_size(p);
+	int i;
+	PyListObject *obj = (pyListObject *)p;
+	printf("[*] size of the python list = %li\n", size);
+	printf("[*] Allocated = %li\n", obj->allocated);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, py_TYPE (obj -> ob_item[i])-> tp_name);
+
+}
