0c2973a72e79cbc7b5df717842ea7a18d6958cce
ImisiDahunsi4 <136653498+ImisiDahunsi4@users.noreply.github.com>
2024-01-10 23:30:01 +0100

    bullshit

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix:
+        new_matrix = [row[:] for row in matrix]
+        for i, row in enumerate(new_matrix):
+            for j, col in enumerate(new_matrix):
+                new_matrix[i][j] = row[j] ** 2
+        return new_matrix
---
+++ 0x04-python-more_data_structures/README.md
@@ -0,0 +1,1 @@
+This delves deeper into python dsa.
