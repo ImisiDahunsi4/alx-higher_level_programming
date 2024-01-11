0c4060e04adf1c1133ddaec14c2b2367ad726f3f
ImisiDahunsi4 <136653498+ImisiDahunsi4@users.noreply.github.com>
2024-01-11 01:10:45 +0100

    cmon nau

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    newdic = a_dictionary
-    for key in newdic:
-        value = newdic[key]
-        newdic[key] = value * 2
+    newdic = {key: value * 2 for key, value in a_dictionary.items()}
     return newdic
