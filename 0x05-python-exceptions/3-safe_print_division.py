37c119772bd49cbbf060ef1bf59ec9d8cf4aa03c
ImisiDahunsi4 <136653498+ImisiDahunsi4@users.noreply.github.com>
2024-01-22 17:33:26 +0100

    exited

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        Result = a / b
-    except:
-        print("Inside result: {}".format(None))
-        return None
-    else:
-        print("Inside result: {}".format(Result))
-        return Result
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    except Exception as e:
+        result = None
+    finally:
+        print("Invalid result: {}".format(result))
+        return result
